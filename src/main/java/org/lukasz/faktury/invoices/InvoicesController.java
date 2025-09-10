087aa3ab5a3e83fd68a1ca59d3db176719d5d602
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-09-10 18:23:17 +0200

    tax,unit

--- src/main/java/org/lukasz/faktury/invoices/InvoicesController.java
+++ src/main/java/org/lukasz/faktury/invoices/InvoicesController.java
@@ -1,9 +1,5 @@
 package org.lukasz.faktury.invoices;
 
-import org.lukasz.faktury.gusapi.NipApiResponse;
-import org.lukasz.faktury.gusapi.Result;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +10,4 @@ public class InvoicesController {
         this.service = service;
     }
 
-    @GetMapping("/{nip}")
-    NipApiResponse ttt(@PathVariable String nip) {
-        return service.result(nip);
-    }
 }
