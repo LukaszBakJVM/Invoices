6db69ca7996b48e43393aab85556a56ac7d48309
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-10-06 21:08:15 +0200

    vaadin prod

--- src/main/java/org/lukasz/faktury/FakturyApplication.java
+++ src/main/java/org/lukasz/faktury/FakturyApplication.java
@@ -1,12 +1,11 @@
 package org.lukasz.faktury;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.vaadin.flow.theme.Theme;
-import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
-@Theme("my-theme")
+
 public class FakturyApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
--- src/test/java/org/lukasz/faktury/FakturyApplicationTests.java
+++ src/test/java/org/lukasz/faktury/FakturyApplicationTests.java
@@ -2,8 +2,10 @@ package org.lukasz.faktury;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("prod")
 class FakturyApplicationTests {
 
     @Test
