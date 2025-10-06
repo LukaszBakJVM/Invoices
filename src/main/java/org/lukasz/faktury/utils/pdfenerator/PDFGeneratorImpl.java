86feaace8c18df7673b74843d51b4372719b26af
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-10-06 16:10:31 +0200

    create Pdf

--- src/main/java/org/lukasz/faktury/utils/pdfenerator/PDFGeneratorImpl.java
+++ src/main/java/org/lukasz/faktury/utils/pdfenerator/PDFGeneratorImpl.java
@@ -1,92 +1,135 @@
 package org.lukasz.faktury.utils.pdfenerator;
 
+import com.itextpdf.io.font.PdfEncodings;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.borders.Border;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
-import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.HorizontalAlignment;
+import com.itextpdf.layout.properties.UnitValue;
+import org.lukasz.faktury.Buyer.dto.BuyerDto;
 import org.lukasz.faktury.invoices.dto.InvoicesDto;
+import org.lukasz.faktury.invoices.dto.InvoicesPdf;
+import org.lukasz.faktury.items.dto.InvoiceItemsDto;
+import org.lukasz.faktury.seller.Seller;
+import org.lukasz.faktury.seller.SellerService;
+import org.lukasz.faktury.views.invoice.TotalValues;
 import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.util.stream.IntStream;
 
 @Component
 public class PDFGeneratorImpl implements PDFGenerator {
-    private final ExecutorService executorService;
+    private final SellerService sellerService;
 
-    public PDFGeneratorImpl() {
-        int numThreads = Runtime.getRuntime().availableProcessors();
-        executorService = Executors.newFixedThreadPool(numThreads);
+
+    public PDFGeneratorImpl(SellerService sellerService) {
+        this.sellerService = sellerService;
 
     }
+
 
     @Override
-
-    public byte[] generatePDFsIn(List<InvoicesDto> invoices) throws InterruptedException, IOException {
-        ByteArrayOutputStream zipStream = new ByteArrayOutputStream();
-        try (ZipOutputStream zipOutputStream = new ZipOutputStream(zipStream)) {
-            for (int i = 0; i < invoices.size(); i++) {
-                InvoicesDto invoice = invoices.get(i);
-                @SuppressWarnings("unused") ByteArrayOutputStream pdfStream = new ByteArrayOutputStream();
-
-                final int index = i;
-                executorService.execute(() -> {
-                    try {
-                        byte[] pdfContent = generatePDF(invoice);
-                        synchronized (zipOutputStream) {
-                            zipOutputStream.putNextEntry(new ZipEntry("invoice_" + (index + 1) + ".pdf"));
-                            zipOutputStream.write(pdfContent);
-                            zipOutputStream.closeEntry();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-            executorService.shutdown();
-            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        }
-        return zipStream.toByteArray();
-    }
-
-    private byte[] generatePDF(InvoicesDto invoice) throws IOException {
+    public byte[] generatePDF(InvoicesPdf invoice) throws IOException {
+        PdfFont font = PdfFontFactory.createFont("fonts/DejaVuSans.ttf", PdfEncodings.IDENTITY_H);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try (PdfWriter writer = new PdfWriter(outputStream); PdfDocument pdfDocument = new PdfDocument(writer); Document document = new Document(pdfDocument)) {
-            addTitleText(document, "Dinamic ttitle invoice 2024");
 
-            addInvoiceTable(document, invoice);
+        try (PdfWriter writer = new PdfWriter(outputStream); PdfDocument pdfDocument = new PdfDocument(writer); Document document = new Document(pdfDocument)) {
+            document.setFont(font);
+
+            Seller seller = sellerService.findByEmail();
+            addInvoiceHeader(document, invoice.invoicesDto());
+            addSellerBuyer(document, seller, invoice.buyerDto(), invoice.invoicesDto().typOfPayment());
+            addInvoiceItems(document, invoice.invoiceItemsDto());
+            addInvoiceSummary(document, invoice.totalValues());
         }
         return outputStream.toByteArray();
     }
 
-    private void addTitleText(Document document, String titleText) {
-        Paragraph paragraph = new Paragraph(titleText).setBold().setFontSize(20).setMarginTop(20);
-        paragraph.setTextAlignment(TextAlignment.CENTER);
-        document.add(paragraph);
+
+    private void addInvoiceHeader(Document document, InvoicesDto invoicesDto) {
+
+        Paragraph invoiceNumber = new Paragraph(String.format("Faktura numer: %s ", invoicesDto.number())).setFontSize(14).setBold();
+        document.add(invoiceNumber);
+
+        // Daty i płatność
+        Paragraph dates = new Paragraph().add(String.format("Data wystawienia:  %s%s Data sprzedaży: %s%s Termin płatności: %s%s Płatność: %s   ", invoicesDto.dateOfIssue(), "\n", invoicesDto.dateOfSale(), "\n", invoicesDto.paymentDate(), "\n", invoicesDto.typOfPayment()));
+        document.add(dates);
+
+        document.add(new Paragraph("\n"));
     }
 
-    private void addInvoiceTable(Document document, InvoicesDto invoice) {
+    private void addSellerBuyer(Document document, Seller seller, BuyerDto buyerDto, String typeOfPayment) {
         Table table = new Table(2);
-        table.addCell("Invoice Number");
+        table.setWidth(UnitValue.createPercentValue(100));
 
-        table.addCell(invoice.number());
-        table.addCell("Client Name");
-        table.addCell(invoice.typOfPayment().toString());
-        table.addCell("Date");
-        table.addCell(invoice.dateOfIssue().toString());
-        table.addCell("Details");
-        com.itextpdf.layout.element.List list = new com.itextpdf.layout.element.List();
-        // invoice..forEach(InvoiceItems::getInvoices);
-        table.addCell(list);
+        // Sprzedawca
+        Cell sellerCell = new Cell().add(new Paragraph(String.format("Sprzedawca %s %s %s %s %s %s %s %s %s NIP %s %s", "\n", seller.getName(), "\n", seller.getStreet(), seller.getHouseNumber(), "\n", seller.getZipCode(), seller.getCity(), "\n", seller.getNip(),"\n")).setBorder(Border.NO_BORDER));
+        if (!typeOfPayment.equals("Gotówka")) {
+            sellerCell.add(new Paragraph(String.format("Nr konta %s ", seller.getAccountNb())));
+        }
+        table.addCell(sellerCell);
+
+        // Nabywca
+        Cell buyerCell = new Cell().add(new Paragraph(String.format("Nabywca %s %s %s %s %s %s %s %s %s NIP %s ", "\n", buyerDto.name(), "\n", buyerDto.street(), buyerDto.houseNumber(), "\n", buyerDto.zipCode(), buyerDto.city(), "\n", buyerDto.nip())).setBorder(Border.NO_BORDER));
+        table.addCell(buyerCell);
+
         document.add(table);
+        document.add(new Paragraph("\n"));
+    }
+
+    private void addInvoiceItems(Document document, List<InvoiceItemsDto> items) {
+        Table table = new Table(new float[]{1, 8, 2, 2, 2, 2, 2, 2});
+        table.setWidth(UnitValue.createPercentValue(100));
+
+        table.addHeaderCell("Lp");
+        table.addHeaderCell("Nazwa");
+        table.addHeaderCell("Ilość");
+        table.addHeaderCell("Jednostka");
+
+        table.addHeaderCell("Cena netto");
+        table.addHeaderCell("VAT");
+        table.addHeaderCell("Cena Brutto");
+        table.addHeaderCell("Wartość brutto");
+
+        IntStream.range(0, items.size()).forEach(i -> {
+            InvoiceItemsDto item = items.get(i);
+            table.addCell(String.valueOf(i + 1));
+            table.addCell(item.description());
+            table.addCell(String.valueOf(item.quantity()));
+            table.addCell(item.unit());
+            table.addCell(String.valueOf(item.priceNetto()));
+            table.addCell(String.valueOf(item.tax()));
+            table.addCell(String.valueOf(item.priceBrutto()));
+            table.addCell(String.valueOf(item.totalValue()));
+        });
+
+        document.add(table);
+    }
+
+    private void addInvoiceSummary(Document document, TotalValues totalValues) {
+        Table summaryTable = new Table(2);
+        summaryTable.setWidth(UnitValue.createPercentValue(40));
+        summaryTable.setHorizontalAlignment(HorizontalAlignment.RIGHT);
+
+        summaryTable.addCell(new Cell().add(new Paragraph("Wartość netto")).setBorder(Border.NO_BORDER));
+        summaryTable.addCell(new Cell().add(new Paragraph(String.valueOf(totalValues.totalNetto()))).setBorder(Border.NO_BORDER));
+
+        summaryTable.addCell(new Cell().add(new Paragraph("VAT")).setBorder(Border.NO_BORDER));
+        summaryTable.addCell(new Cell().add(new Paragraph(String.valueOf(totalValues.totalVat()))).setBorder(Border.NO_BORDER));
+
+        summaryTable.addCell(new Cell().add(new Paragraph("Wartość brutto")).setBorder(Border.NO_BORDER));
+        summaryTable.addCell(new Cell().add(new Paragraph(String.valueOf(totalValues.totalBrutto()))).setBorder(Border.NO_BORDER));
+
+        document.add(summaryTable);
     }
 }
 
