0762fdc9ba53f8cccf57dd17173205c39bd9cc8d
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-10-08 11:06:49 +0200

    test  config

--- src/test/java/org/lukasz/faktury/FakturyApplicationTests.java
+++ src/test/java/org/lukasz/faktury/FakturyApplicationTests.java
@@ -1,13 +1,60 @@
 package org.lukasz.faktury;
 
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+
 class FakturyApplicationTests {
+    @MockitoBean
+    private JavaMailSender javaMailSender;
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest").withDatabaseName("invoices").withUsername("test").withPassword("test");
+
+
+
+    @RegisterExtension
+    static WireMockExtension wireMockServer = WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
+
+    @LocalServerPort
+    int dynamicPort;
+
+    @DynamicPropertySource
+    static void registerDynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("nipApi", wireMockServer::baseUrl);
+        registry.add("tokenUrl",wireMockServer::baseUrl);
+        registry.add("spring.datasource.url", () -> postgreSQLContainer.getJdbcUrl());
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @BeforeAll
+    static void startPostgres() {
+        postgreSQLContainer.start();
+
+
+    }
+
+    @AfterAll
+    static void stopPostgres() {
+        postgreSQLContainer.stop();
+    }
 
     @Test
     void contextLoads() {
-    }
 
+    }
 }
