cd368f5985df416ba86bbcf5287594d3fce60eec
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-10-15 14:26:32 +0200

    api  ceidg and mf more than 1 company

--- src/main/java/org/lukasz/faktury/seller/SellerService.java
+++ src/main/java/org/lukasz/faktury/seller/SellerService.java
@@ -3,9 +3,10 @@ package org.lukasz.faktury.seller;
 import java.util.List;
 
 public interface SellerService {
-   List< Seller> save (List<SellerDto> dto);
+    Seller save(SellerDto dto);
     SellerDto findByUserEmail();
     Seller findByEmail();
     void addAccountNb(String nb);
+    SellerDto selectCompany(List<SellerDto> companies,String company);
 
 }
